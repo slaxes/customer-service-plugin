631aad4a04af678e48e9e993b0670ab59e4f14e2
Xiaoguang Zhu <694066681@qq.com>
2019-04-12 14:05:40 +0800

    Add files via upload

--- scripts/sqlite2xlsx.py
+++ scripts/sqlite2xlsx.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
 
+__help_text__ = '''
+Usage:
+
+    python sqlite2xlsx.py input_db [recall_db] output_xlsx
+
+Parameters:
+- input_db Dataloader.to_sqlite() 导出的数据库文件
+- recall_db [可选] RecallData 维护的召回数据库文件
+- output_xlsx 输出的 Excel 文件
+
+输出文件中将含有以下几张表（sheet）：
+- 知识库 线上模型数据集
+
+    | 主问题 | 标准答案 | 问法 |
+
+- labeled RecallData 数据库中 labeled 表，包含打标后的数据
+
+    | 问法 | 标注答案 |
+
+- recall RecallData 数据库中 recall 表
+
+    | 问法 |
+'''
+
 import sqlite3
 import openpyxl as xl
 from openpyxl.styles import Font as xlfont
@@ -8,11 +32,20 @@ import sys
 import os
 
 
-if not len(sys.argv) == 3:
-    print('Usage: python sqlite2xlsx.py input_db output_xlsx')
-    exit(-1)
+''' Command Line Argument Parsing '''
 
-input_db_path, output_xlsx_path = sys.argv[1:]
+input_db_path, recall_db_path, output_xlsx_path = [None,] * 3
+
+if '-h' in sys.argv or '--help' in sys.argv:
+    print(__help_text__)
+    exit(0)
+elif len(sys.argv) == 3:
+    input_db_path, output_xlsx_path = sys.argv[1:]
+elif len(sys.argv) == 4:
+    input_db_path, recall_db_path, output_xlsx_path = sys.argv[1:]
+else:
+    print('Usage: python sqlite2xlsx.py input_db [recall_db] output_xlsx')
+    exit(-1)
 
 if not os.path.exists(input_db_path):
     print('"{}" not found!'.format(input_db_path))
@@ -23,11 +56,16 @@ if os.path.exists(output_xlsx_path):
         pass
     else:
         exit(-1)
+if recall_db_path is not None and not os.path.exists(recall_db_path):
+    print('Recall database at "{}" not found!'.format(recall_db_path))
+    exit(-1)
 
+
+''' Exporting Active Knowledge Base '''
 
 workbook = xl.Workbook()
 worksheet = workbook.active
-worksheet.title = 'xxxxxxx'
+worksheet.title = '知识库'
 worksheet.cell(1, 1, value='主问题')
 worksheet.cell(1, 2, value='标准答案')
 worksheet.cell(1, 3, value='问法')
@@ -62,5 +100,39 @@ with sqlite3.connect(input_db_path) as conn:
             col_idx,
             value=ques
         )
+
+workbook.save(output_xlsx_path)
+
+
+''' Exporting Recall Database '''
+
+if not recall_db_path:
+    exit(0)
+
+# export `labeled` table
+
+worksheet = workbook.create_sheet('labeled')
+worksheet.cell(1, 1, value='问法')
+worksheet.cell(1, 2, value='标注答案')
+
+with sqlite3.connect(recall_db_path) as conn:
+    cur = conn.execute('SELECT `ques`, `ans` FROM `labeled`')
+    labeled = cur.fetchall()
+    for row_id, (ques, ans) in enumerate(labeled):
+        worksheet.cell(row_id + 2, 1, value=ques)
+        worksheet.cell(row_id + 2, 2, value=ans)
+    del labeled
+
+# export `recall` table
+
+worksheet = workbook.create_sheet('recall')
+worksheet.cell(1, 1, value='问法')
+
+with sqlite3.connect(recall_db_path) as conn:
+    cur = conn.execute('SELECT `ques` FROM `recall`')
+    recall = cur.fetchall()
+    for row_id, (ques,) in enumerate(recall):
+        worksheet.cell(row_id + 2, 1, value=ques)
+    del recall
 
 workbook.save(output_xlsx_path)
